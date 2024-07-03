e4cf57d3666a94df0373ec0f378b91699e8d1ffa
wicaksu <wicaksono.bayuaji.wba@gmail.com>
2024-07-03 20:52:51 +0700

    ok

--- engine/Telegram/helper.js
+++ engine/Telegram/helper.js
@@ -2,17 +2,15 @@ const UserModel = require("../models/userModel");
 const UserAutoAbsenModel = require("../models/userAutoAbsenModel");
 
 const Switch = async (data, bot) => {
-  let { text, user } = "";
   const { message, callback_query } = data;
   const botData = message || callback_query.message;
   const { first_name, last_name, username, id, type } = botData.chat;
   const isGroup = type !== "private";
-  const groupName =
-    type === "private" ? `${first_name} ${last_name}` : message?.chat.title;
+  const groupName = isGroup
+    ? message?.chat.title
+    : `${first_name} ${last_name}`;
 
-  if (callback_query.data) text = callback_query.data + " ";
-  else text = botData.text + " ";
-
+  let text = callback_query?.data || botData.text || "";
   const key = text.split(" ")[0];
   const msg = text.slice(key.length + 1).trim();
   let replay = "";
@@ -49,36 +47,25 @@ const Switch = async (data, bot) => {
         ? `Nama Grub : ${groupName}\nChat Id : ${id}\n`
         : `Nama : ${groupName}\nChat Id : ${id}\n`;
       break;
-
     case "/saldo":
-      user = await UserModel.findOne({ chatIdTelegram: id });
+      const user = await UserModel.findOne({ chatIdTelegram: id });
       if (user) {
         const { name, balance, nip, _id } = user;
-        let userAutoAbsen = await UserAutoAbsenModel.findOne({ user: _id });
-        if (userAutoAbsen) {
-          replay = `*Hai ${name}*\n`;
-          replay += `NIP anda : ${nip}\n`;
-          replay += `Imei anda : ${userAutoAbsen.imei}\n`;
-          replay += `Url layanan : ${userAutoAbsen.url}\n`;
-          replay += `Sisa saldo : ${balance}\n`;
-        } else {
-          replay = `*Hai ${name}*\n`;
-          replay += `Anda belum terdaftar pada layanan apapun!`;
-        }
+        const userAutoAbsen = await UserAutoAbsenModel.findOne({ user: _id });
+        replay = userAutoAbsen
+          ? `*Hai ${name}*\nNIP anda : ${nip}\nImei anda : ${userAutoAbsen.imei}\nUrl layanan : ${userAutoAbsen.url}\nSisa saldo : ${balance}\n`
+          : `*Hai ${name}*\nAnda belum terdaftar pada layanan apapun!`;
       } else {
         replay =
           "*Akun anda belum terdaftar*\n\nApabila anda tertarik dengan layanan ini silahkan menghubungi @wicakbay";
       }
       break;
-
     case "/cekin":
       replay = `Absen masuk ${msg}`;
       break;
-
     case "/cekout":
       replay = `Absen pulang ${msg}`;
       break;
-
     default:
       replay = "*Command tidak tersedia*";
       break;
